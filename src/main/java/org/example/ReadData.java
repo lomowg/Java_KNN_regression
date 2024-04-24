93d5a659bdda53c241337bbe466b872569392fab
Lomowg <sokol789123@yandex.ru>
2024-04-24 15:44:34 +0300

    Changed protection methods

--- src/main/java/org/example/ReadData.java
+++ src/main/java/org/example/ReadData.java
@@ -3,15 +3,15 @@ package org.example;
 import java.io.*;
 
 public class ReadData {
-    static String header;
+    static public String header;
     static double[] temperature = new double[1455324];
     static double[] pressure = new double[1455324];
     static double[] wind = new double[1455324];
     static double[] snow = new double[1455324];
     static double[] precipitation = new double[1455324];
 
-    static double[][] table_target = new double[5][1455324];
-    static String[][] table = new String[6][1455324];
+    static public double[][] table_target = new double[5][1455324];
+    static public String[][] table = new String[6][1455324];
 
     public static void main(String[] args) {
 
