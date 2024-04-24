221199b3924ab3b48c4c342fc03a22f057c15a9b
Lomowg <sokol789123@yandex.ru>
2024-04-24 15:43:50 +0300

    Added write data class

---
+++ src/main/java/org/example/WriteData.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriteData {
+
+    public static void write(String csvFilePath, String Header, String[][] table, double[][] table_target) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
+        writer.write(Header + "\n");
+
+        for (int row = 0; row < table[0].length; row++) {
+
+            writer.write(table[0][row]);
+            for (int col = 1; col < table.length; col++) {
+                writer.write("," + table[col][row]);
+            }
+
+            for (double[] doubles : table_target) {
+                writer.write("," + doubles[row]);
+            }
+
+            writer.write("\n");
+        }
+
+        writer.close();
+    }
+}
