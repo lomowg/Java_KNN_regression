e90dd26c228b73c6bb4793ae9abd6810783066bb
Lomowg <sokol789123@yandex.ru>
2024-04-28 11:06:51 +0300

    Added main code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,174 +1,35 @@
 package org.example;
 
-import java.io.*;
-public class Main {
 
-    public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) throws Exception {
         String csvFilePath = "Data_2.csv";
 
-        normalizeData(csvFilePath);
+        ReadData rd = new ReadData();
+        WriteData wd = new WriteData();
+        Preparing prep = new Preparing();
+        KNN KNNRegression = new KNN();
 
-        denormalizeData(csvFilePath + "_normalized.csv");
-    }
+        rd.load(csvFilePath);
 
-    public static void normalizeData(String csvFilePath) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(csvFilePath));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath + "_normalized.csv"));
+        double[][] targets = rd.table_target;
 
-            String header = reader.readLine();
-            writer.write(header + "\n");
+        double[] mean_val = new double[targets.length];
+        double[] std_val = new double[targets.length];
 
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = splitStringIntoValues(line);
-                double[] values = new double[parts.length - 6]; // Учитываем только нужные столбцы
-                for (int i = 6; i < parts.length; i++) {
-                    if (!parts[i].isEmpty()) {
-                        values[i - 6] = Double.parseDouble(parts[i]);
-                    } else {
-                        // Заменяем пропуски на NaN
-                        values[i - 6] = Double.NaN;
-                    }
-                }
-
-                double[] normalizedValues = normalize(values);
-
-                writer.write(parts[0]);
-                for (int i = 1; i < 6; i++){
-                    writer.write("," + parts[i]);
-                }
-
-                for (double val : normalizedValues) {
-                    writer.write("," + val);
-                }
-                writer.write("\n");
-            }
-
-            reader.close();
-            writer.close();
-
-            System.out.println("Normalization completed.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void denormalizeData(String normalizedCsvFilePath) {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(normalizedCsvFilePath));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(normalizedCsvFilePath.replace("_normalized.csv", "_denormalized.csv")));
-
-            String header = reader.readLine();
-            writer.write(header + "\n");
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = splitStringIntoValues(line);
-                double[] values = new double[parts.length - 6]; // Учитываем только нужные столбцы
-                for (int i = 6; i < parts.length; i++) {
-                    if (!parts[i].isEmpty()) {
-                        values[i - 6] = Double.parseDouble(parts[i]);
-                    }
-                    else {
-                        // Заменяем пропуски на NaN
-                        values[i - 6] = Double.NaN;
-                    }
-                }
-
-                double[] denormalizedValues = denormalize(values);
-
-                writer.write(parts[0]);
-                for (int i = 1; i < 6; i++){
-                    writer.write("," + parts[i]);
-                }
-
-                for (double val : denormalizedValues) {
-                    writer.write("," + val);
-                }
-                writer.write("\n");
-            }
-
-            reader.close();
-            writer.close();
-
-            System.out.println("Denormalization completed.");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static double[] normalize(double[] data) {
-
-        double meanValue = mean(data);
-        double stdDevValue = stdDev(data);
-
-        double[] normalizedData = new double[data.length];
-        for (int i = 0; i < data.length; i++) {
-            if (!Double.isNaN(data[i])) {
-                normalizedData[i] = (data[i] - meanValue) / stdDevValue;
-            } else {
-                // Пропуск не требует нормализации
-                normalizedData[i] = Double.NaN;
-            }
+        for (int col = 0; col < targets.length; col++) {
+            targets[col] = prep.normalize(targets[col]);
+            mean_val[col] = prep.meanValue;
+            std_val[col] = prep.stdDevValue;
         }
 
-        return normalizedData;
-    }
+        double[][] knn_targets = KNNRegression.train(targets);
 
-    public static double[] denormalize(double[] normalizedData) {
-
-        double meanValue = mean(normalizedData);
-        double stdDevValue = stdDev(normalizedData);
-
-        double[] denormalizedData = new double[normalizedData.length];
-        for (int i = 0; i < normalizedData.length; i++) {
-            if (!Double.isNaN(normalizedData[i])) {
-                denormalizedData[i] = (normalizedData[i] * stdDevValue) + meanValue;
-            } else {
-                // Пропуск не требует денормализации
-                denormalizedData[i] = Double.NaN;
-            }
+        for (int col = 0; col < knn_targets.length; col++) {
+            knn_targets[col] = prep.denormalize(knn_targets[col], mean_val[col], std_val[col]);
         }
 
-        return denormalizedData;
-    }
+        wd.write("KNN_test.csv", rd.header, rd.table, knn_targets);
 
-    public static String[] splitStringIntoValues(String input) {
-        String[] parts = input.split(",", -1); // -1 argument to include trailing empty strings
-
-        // Ensure there are at least 11 parts
-        if (parts.length < 11) {
-            // Add empty values if needed
-            int emptyCount = 11 - parts.length;
-            for (int i = 0; i < emptyCount; i++) {
-                input += ",";
-            }
-            parts = input.split(",", -1);
-        }
-
-        return parts;
-    }
-
-    public static double mean(double[] data) {
-        double sum = 0;
-        for (double value : data) {
-            if (!Double.isNaN(value)) {
-                sum += value;
-            }
-        }
-        return sum / data.length;
-    }
-
-    public static double stdDev(double[] data) {
-        double mean = mean(data);
-        double sumSquaredDiff = 0;
-        for (double value : data) {
-            if (!Double.isNaN(value)) {
-                double diff = value - mean;
-                sumSquaredDiff += diff * diff;
-            }
-        }
-        return Math.sqrt(sumSquaredDiff / data.length);
     }
 }
