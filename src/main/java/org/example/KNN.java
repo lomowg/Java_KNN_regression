ea01913b18fc1d8fddd59a28b75dd638146a284e
Lomowg <sokol789123@yandex.ru>
2024-05-11 15:37:48 +0300

    Created working KNN

--- src/main/java/org/example/KNN.java
+++ src/main/java/org/example/KNN.java
@@ -1,19 +1,16 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class KNN {
 
-    private List<double[]> data;
+    private double[][] data;
 
-    public KNN(List<double[]> data) {
+    public KNN(double[][] data) {
         this.data = data;
     }
 
     public void fillMissingValues() {
-        for (int i = 0; i < data.size(); i++) {
-            double[] instance = data.get(i);
+        for (int i = 0; i < data.length; i++) {
+            double[] instance = data[i];
             for (int j = 0; j < instance.length; j++) {
                 if (Double.isNaN(instance[j])) {
                     double nearestNeighborValue = findNearestNeighborValue(i, j);
@@ -27,10 +24,10 @@ public class KNN {
         double minDistance = Double.MAX_VALUE;
         double nearestNeighborValue = 0.0;
 
-        for (int i = 0; i < data.size(); i++) {
+        for (int i = 0; i < data.length; i++) {
             if (i != rowIndex) {
-                double[] neighbor = data.get(i);
-                double distance = euclideanDistance(data.get(rowIndex), neighbor);
+                double[] neighbor = data[i];
+                double distance = euclideanDistance(data[rowIndex], neighbor);
 
                 if (distance < minDistance && !Double.isNaN(neighbor[columnIndex])) {
                     minDistance = distance;
@@ -50,24 +47,5 @@ public class KNN {
             }
         }
         return Math.sqrt(distance);
-    }
-
-    public static void main(String[] args) {
-        List<double[]> data = new ArrayList<>();
-        data.add(new double[]{25.0, 1013.2, 10.0, 5.0, 20.0});
-        data.add(new double[]{20.0, Double.NaN, 8.0, 3.0, 15.0});
-        data.add(new double[]{22.0, 1015.5, 12.0, Double.NaN, 18.0});
-        data.add(new double[]{12.0, Double.NaN, 10.0, Double.NaN, Double.NaN});
-        data.add(new double[]{22.0, 1013.2, Double.NaN, Double.NaN, Double.NaN});
-
-        KNN imputer = new KNN(data);
-        imputer.fillMissingValues();
-
-        for (double[] instance : data) {
-            for (double value : instance) {
-                System.out.print(value + "\t");
-            }
-            System.out.println();
-        }
     }
 }
